48c6014a14c48c7cf5f1c396184e98391d1db52b
Sandip <sandip.2bhatta@gmail.com>
2024-11-24 08:55:33 +0545

    Test

--- api/app.js
+++ api/app.js
@@ -3,10 +3,10 @@ import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 
 const app = express();
+console.log("sandip");
 
 app.use("/api/posts", postRoute);
 
-
-app.listen(8800, ()=>{
-    console.log("Backend server is running on port 8800");
-}); 
+app.listen(8800, () => {
+  console.log("Backend server is running on port 8800");
+});
